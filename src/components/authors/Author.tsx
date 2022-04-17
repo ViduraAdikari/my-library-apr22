cd8b5ebf7f668b7278603a6ce4cf31f9a8e9cc54
Vidura Adikari <vidura.ce@gmail.com>
2022-04-17 17:07:39 +0530

    Create, List and Delete Author

--- src/components/authors/Author.tsx
+++ src/components/authors/Author.tsx
@@ -4,6 +4,7 @@ import {Col, Row} from "react-bootstrap";
 type AuthorProps = {
   num: number
   authorName: string
+  onDeleteClick: (indexToDelete: number) => void
 }
 
 const Author: React.FC<AuthorProps> = (props) => {
@@ -17,7 +18,7 @@ const Author: React.FC<AuthorProps> = (props) => {
       </Col>
       <Col xs={3} className='d-flex justify-content-end align-items-center'>
         <i className='feather-edit text-warning me-3'/>
-        <i className='feather-trash-2 text-danger me-3'/>
+        <i className='feather-trash-2 text-danger me-3' onClick={() => props.onDeleteClick(num - 1)}/>
       </Col>
     </Row>
   )
